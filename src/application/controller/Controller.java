d966cc0e3c6b7cb299decfa7166910d228442c35
Elizalll <1260939779@qq.com>
2022-11-22 09:22:49 +0800

    first complete version2

--- src/application/controller/Controller.java
+++ src/application/controller/Controller.java
@@ -1,5 +1,6 @@
 package application.controller;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
@@ -13,7 +14,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.ConnectException;
 import java.net.Socket;
+import java.net.SocketException;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.Objects;
@@ -27,11 +30,13 @@ public class Controller implements Initializable {
     private static final int BOUND = 90;
     private static final int OFFSET = 15;
     public Label label;
+    public Label winLabel;
+    public Label nameLabel;
     @FXML
     private Pane base_square;
     @FXML
     private Rectangle game_panel;
-    private static boolean TURN = true, GAME_START = false, TURN_GAME = false;
+    private static boolean TURN = true, GAME_START = false, GAME_END = false;
     private static final int[][] chessBoard = new int[3][3];
     private static final boolean[][] flag = new boolean[3][3];
 
@@ -42,129 +47,198 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        try {
-            socket = new Socket("localhost", 1234);
-            pw = new PrintWriter(socket.getOutputStream());
-            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            pw.write("Client " + name + " here, I wants to play.\nEND\n");
-            pw.flush();
-            System.out.println(ReadOneSentence(br));// First sentence from the server
-            String info;
-            do {
-                info = ReadOneSentence(br);
-                if (info.length() > 0) {
-                    System.out.println(name + " " + info);
-                }
-            } while (!info.equals("Matched."));
-
-            info = ReadOneSentence(br);
-            if (Objects.equals(info, "Player 1")) {
-                I_AM = PLAY_1;
-                System.out.println("I am player 1");
-                showTurnInfo();
-                GAME_START = true;
-            } else if (Objects.equals(info, "Player 2")) {
-                I_AM = PLAY_2;
-                TURN = false;
-                System.out.println("I am player 2");
-                showTurnInfo();
-                if (!TURN && !GAME_START) {
-                    GAME_START = true;
-                    System.out.println("GAME_START = true;");
-                    String[] infoij;
-                    try {
-                        infoij = ReadOneSentence(br).split(",");
-                        System.out.println("info0" + Arrays.toString(infoij));
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                    int i = Integer.parseInt(infoij[0]), j = Integer.parseInt(infoij[1]);
-                    if (refreshBoard(i, j)) {
-                        TURN = true;
-                        showTurnInfo();
-                    }
-                }
-            } else {
-                System.out.println("Wrong in I_AM");
-            }
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-
+        new socketThread().start();
         game_panel.setOnMouseClicked(event -> {
-            if (TURN && GAME_START) {
+            if (TURN && GAME_START && !GAME_END) {
                 int x = (int) (event.getX() / BOUND);
                 int y = (int) (event.getY() / BOUND);
                 if (refreshBoard(x, y)) {
                     TURN = false;
                     showTurnInfo();
-                    pw.write(x + "," + y + "\nEND\n");
-                    pw.flush();
-                    if (!TURN && GAME_START) {
-                        try {
-                            String[] infoij;
-                            String info;
-                            info = ReadOneSentence(br);
-                            System.out.println("info1:" + info);
-
-                            switch (info) {
-                                case "Continue" -> {
-                                    info = ReadOneSentence(br);
-                                    System.out.println("info2:" + info);
-                                    infoij = info.split(",");
-                                    int i = Integer.parseInt(infoij[0]), j = Integer.parseInt(infoij[1]);
-                                    if (refreshBoard(i, j)) {
-
-                                        info = ReadOneSentence(br);
-                                        System.out.println("info3:" + info);
-                                        switch (info) {
-                                            case "Continue" -> {
-                                                TURN = true;
-                                                showTurnInfo();
-                                            }
-                                            case "Win" -> showWinInfo(true);
-                                            case "Loss" -> showWinInfo(false);
-                                            default -> System.out.println("Wrong in inner Win switch");
-                                        }
-                                    }
-                                }
-                                case "Win" -> showWinInfo(true);
-                                case "Loss" -> showWinInfo(false);
-                                default -> System.out.println("Wrong in Win switch");
-                            }
-
-                        } catch (IOException | InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
+                    new socketThread2(x, y).start();
                 }
             }
         });
     }
 
+    class socketThread extends Thread {
+        @Override
+        public void run() {
+            try {
+                socket = new Socket("localhost", 1234);
+                pw = new PrintWriter(socket.getOutputStream());
+                br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                pw.write("Client " + name + " here, I wants to play.\nEND\n");
+                pw.flush();
+                System.out.println(ReadOneSentence(br));// First sentence from the server
+                String info;
+                do {
+                    info = ReadOneSentence(br);
+                    if (info.length() > 0) {
+                        System.out.println(name + " " + info);
+                    }
+                } while (!info.equals("Matched."));
 
-    String ReadOneSentence(BufferedReader br) throws IOException, InterruptedException {
+                info = ReadOneSentence(br);
+                if (Objects.equals(info, "Player 1")) {
+                    I_AM = PLAY_1;
+                    System.out.println("I am player 1");
+                    showTurnInfo();
+                    showPlayerInfo();
+                    GAME_START = true;
+                } else if (Objects.equals(info, "Player 2")) {
+                    I_AM = PLAY_2;
+                    TURN = false;
+                    System.out.println("I am player 2");
+                    showTurnInfo();
+                    showPlayerInfo();
+                    if (!TURN && !GAME_START) {
+                        GAME_START = true;
+                        System.out.println("GAME_START = true;");
+                        String[] infoij;
+
+                        infoij = ReadOneSentence(br).split(",");
+                        System.out.println("info0" + Arrays.toString(infoij));
+                        System.out.println("info0" + ReadOneSentence(br));
+
+                        int i = Integer.parseInt(infoij[0]), j = Integer.parseInt(infoij[1]);
+                        if (refreshBoard(i, j)) {
+                            TURN = true;
+                            showTurnInfo();
+                        }
+                    }
+                } else {
+                    System.out.println("Wrong in I_AM");
+                }
+            } catch (SocketException e) {
+                System.err.println("Oppo/Server Exit");
+//                    System.err.println(e.getMessage());
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                System.err.println("Opposite log out");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    class socketThread2 extends Thread {
+        int x, y;
+
+        socketThread2(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public void run() {
+            pw.write(x + "," + y + "\nEND\n");
+            pw.flush();
+            if (!TURN && GAME_START && !GAME_END) {
+                try {
+                    String[] infoij;
+                    String info;
+                    info = ReadOneSentence(br);
+                    System.out.println("info1:" + info);
+
+                    switch (info) {
+                        case "Continue" -> {
+                            info = ReadOneSentence(br);
+                            System.out.println("info2:" + info);
+                            infoij = info.split(",");
+                            int i = Integer.parseInt(infoij[0]), j = Integer.parseInt(infoij[1]);
+                            if (refreshBoard(i, j)) {
+
+                                info = ReadOneSentence(br);
+                                System.out.println("info3:" + info);
+                                switch (info) {
+                                    case "Continue" -> {
+                                        TURN = true;
+                                        showTurnInfo();
+                                    }
+                                    case "Win" -> {
+                                        GAME_END = true;
+                                        showWinInfo(1);
+                                    }
+                                    case "Loss" -> {
+                                        GAME_END = true;
+                                        showWinInfo(2);
+                                    }
+                                    case "Draw" -> {
+                                        GAME_END = true;
+                                        showWinInfo(0);
+                                    }
+                                    default -> System.out.println("Wrong in inner Win switch");
+                                }
+                            }
+                        }
+                        case "Win" -> {
+                            GAME_END = true;
+                            showWinInfo(1);
+                        }
+                        case "Loss" -> {
+                            GAME_END = true;
+                            showWinInfo(2);
+                        }
+                        case "Draw" -> {
+                            GAME_END = true;
+                            showWinInfo(0);
+                        }
+                        default -> System.out.println("Wrong in Win switch");
+                    }
+
+                } catch (SocketException e) {
+                    System.err.println("Oppo/Server Exit");
+//                    System.err.println(e.getMessage());
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                    System.err.println("Opposite log out");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    String ReadOneSentence(BufferedReader br) throws IOException {
         String line;
         StringBuilder sb = new StringBuilder();
         while ((line = br.readLine()) != null && !"END".equals(line)) {
             sb.append(line);
-            Thread.sleep(100);
         }
-        return sb.toString();
+        line = sb.toString();
+        if (line.equals("Oppo Exit") || line.length() == 0) {
+            throw new SocketException();
+        }
+        return line;
     }
 
     void showTurnInfo() {
-        label.setText("Turn: " + (TURN ? "your" : "opposite"));
+        Platform.runLater(() -> {
+            label.setText("Turn: " + (TURN ? "your" : "opposite"));
+        });
     }
 
-    void showWinInfo(boolean win) {
-        label.setText("Winner: " + (win ? "you" : "opposite"));
+    void showPlayerInfo() {
+        Platform.runLater(() -> {
+            nameLabel.setText("You are " + (I_AM == 1 ? "Circle" : "Cross"));
+        });
+    }
+
+    void showWinInfo(int win) {
+        Platform.runLater(() -> {
+            switch (win) {
+                case 0 -> winLabel.setText("---------Draw---------");
+                case 1 -> winLabel.setText("---Winner :  you! Congratulation!---");
+                case 2 -> winLabel.setText("---Winner :  opposite---");
+            }
+        });
     }
 
     private boolean refreshBoard(int x, int y) {
         if (chessBoard[x][y] == EMPTY) {
             chessBoard[x][y] = TURN ? PLAY_1 : PLAY_2;
-            drawChess();
+            Platform.runLater(this::drawChess);
             return true;
         }
         return false;
@@ -179,10 +253,16 @@ public class Controller implements Initializable {
                 }
                 switch (chessBoard[i][j]) {
                     case PLAY_1:
-                        drawCircle(i, j);
+                        switch (I_AM) {
+                            case 1 -> drawCircle(i, j);
+                            case 2 -> drawLine(i, j);
+                        }
                         break;
                     case PLAY_2:
-                        drawLine(i, j);
+                        switch (I_AM) {
+                            case 1 -> drawLine(i, j);
+                            case 2 -> drawCircle(i, j);
+                        }
                         break;
                     case EMPTY:
                         // do nothing
